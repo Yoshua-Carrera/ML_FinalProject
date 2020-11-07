a5d59d73fc4f0df4bd2561d3e2c426b8b84fc8c9
Yoshua J Carrera <yoshua.carrera24@gmail.com>
2020-11-07 12:27:39 -0500

    Added neural network

--- models.py
+++ models.py
@@ -8,6 +8,11 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
 from sklearn.svm import SVC
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import metrics
 
 df = pd.read_csv('LOL/Clean_LeagueofLegends.csv')
 
@@ -150,12 +155,26 @@ Target:
     rResult: 1 if red team won (boolean)
 '''
 
-# rForest = (n_estimators=100)
+scalar = MinMaxScaler()
+scalar.fit(x)
 
-# rForest.fit(x_train, y_train)
+x_nn = scalar.transform(x)
 
-# y_pred = rForest.predict(x_test)
+x_train, x_test, y_train, y_test = train_test_split(x_nn, y, test_size=0.2, random_state=0)
 
-# rForest_accuracy = sum(y_pred==y_test)/len(y_test)
+nn = Sequential()
 
-# print('Decision tree model accuracy: {}'.format(rForest_accuracy)) # 0.7276902887139107
+nn.add(Dense(10, input_dim=len(x_train[0]), activation='relu'))
+nn.add(Dense(50, activation='relu'))
+nn.add(Dense(10, activation='relu'))
+nn.add(Dense(5, activation='relu'))
+nn.add(Dense(1, activation='sigmoid'))
+
+nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+nn.summary()
+
+history = nn.fit(x_train, y_train, epochs=500, batch_size=10) # 0.9826 (500 iterations)
+
+print(history.history.keys())
+
+nn.evaluate(x_test, y_test) 
